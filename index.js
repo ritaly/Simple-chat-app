b292a8f4e65547b826eb58c56ce9e66ee019ea1e
Malgorzata Rita <malgorzata.lyczywek@gmail.com>
2017-05-15 16:25:05 +0200

    quite good

---
+++ index.js
@@ -0,0 +1,49 @@
+var express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server);
+mongo = require('mongodb').MongoClient;
+
+server.listen(3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
+mongo.connect('mongodb://127.0.0.1/test', function(err, db) {
+  if (err) throw err;
+
+  io.sockets.on('connection', function(socket) {
+    console.log("Socket connected.");
+    var col = db.collection('messages');
+
+    col.find().toArray(function(err, res) {
+      if (err) throw err;
+      socket.emit('output', res);
+    });
+
+    socket.on('message', function(msg) {
+
+      var whitespacePattern = /^\s*$/;
+
+      if (whitespacePattern.test(msg.username) || whitespacePattern.test(msg.message)) {
+        socket.emit('er', "Wiadomość i nazwa użytkownika nie może być pusta.");
+      } else {
+        col.insert({
+          username: msg.username,
+          message: msg.message
+        })
+        io.emit('message', {
+          message: msg.message,
+          username: msg.username
+        });
+      }
+
+    });
+
+  });
+
+});
